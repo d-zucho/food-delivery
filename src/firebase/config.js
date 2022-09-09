b843efe68382a9af7ca3a2f3d55783a64028f1c4
D_Zucho <danny.nico.dev@outlook.com>
2022-09-09 02:48:25 -0700

    initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCXZ9EIhvxqzMW0b8LVwxZzfwZ39cy6GFc',
+  authDomain: 'food-delivery-e3c0b.firebaseapp.com',
+  databaseURL: 'https://food-delivery-e3c0b-default-rtdb.firebaseio.com',
+  projectId: 'food-delivery-e3c0b',
+  storageBucket: 'food-delivery-e3c0b.appspot.com',
+  messagingSenderId: '442734327868',
+  appId: '1:442734327868:web:65225e36f03add94fceb39',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
